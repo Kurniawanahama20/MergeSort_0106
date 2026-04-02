826c7033dc54b9653c542081d01e555148e95b37
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-04-02 10:36:44 +0700

    commit5

--- MergeSort_0106.cpp
+++ MergeSort_0106.cpp
@@ -38,3 +38,13 @@ void mergeSort(int low, int high)
     return; // step 1.a
 
     int mid = (low + high) / 2; // step 2
+
+    // step 3
+    // fungsi rekursif - memanggil diri sendiri
+    mergeSort(low, mid); // step 3.a
+    mergeSort(mid + 1, high); // step 3.b
+
+    // step 4
+    int i = low; // step 4.a
+    int j = mid + 1; // step 4.b
+    int k = low; // step 4.c
